338a5eda02925dd4859b5717431eb35194a072d4
OnceAShadow <fred.sevillano@gmail.com>
2018-05-20 19:33:00 -0400

    First Commit, clean

--- Thread_It/New Group/ViewController.swift
+++ Thread_It/New Group/ViewController.swift
@@ -1,10 +1,8 @@
-//
 //  ViewController.swift
 //  Thread_It
 //
 //  Created by Fred Sevillano on 5/20/18.
 //  Copyright © 2018 TimeTraveler. All rights reserved.
-//
 
 import UIKit
 
@@ -12,14 +10,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
