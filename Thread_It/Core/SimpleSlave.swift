7b0609a7e518bff1712b4c0310c795c03428fdb5
OnceAShadow <fred.sevillano@gmail.com>
2018-05-20 19:38:14 -0400

    Slave added

---
+++ Thread_It/Core/SimpleSlave.swift
@@ -0,0 +1,13 @@
+//  SimpleSlave.swift
+//  Thread_It
+//
+//  Created by Fred Sevillano on 5/20/18.
+//  Copyright © 2018 TimeTraveler. All rights reserved.
+
+import Foundation
+
+class SimpleSlave {
+    
+    
+    
+}
